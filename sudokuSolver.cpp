e7e564e866f660c489299e9e24275a1814a8acb2
purbendu <88208759+purbendu@users.noreply.github.com>
2022-10-11 18:16:53 +0530

    Updated sudokuSolver.cpp

--- sudokuSolver.cpp
+++ sudokuSolver.cpp
@@ -4,50 +4,32 @@ https://leetcode.com/problems/sudoku-solver/
 Leetcode Hard
 */
 
-#include<bits/stdc++.h>
-using namespace std;
-
-
-bool isValidSudoku(vector<vector<char> >& board, int i, int j, char val) {
-    int row = i - i%3, column = j - j%3;
-    for(int x=0; x<9; x++) if(board[x][j] == val) return false;
-    for(int y=0; y<9; y++) if(board[i][y] == val) return false;
-    for(int x=0; x<3; x++) {
-        for(int y=0; y<3; y++) if(board[row+x][column+y] == val) return false;
-    }
-    return true;
-}
-bool helper(vector<vector<char> >& board, int i, int j) {
-    if(i == 9) return 1;
-    if(j == 9) return helper(board,i+1,0);
-    if(board[i][j] != '.') return helper(board,i,j+1);
-    
-    for(char num = '1'; num <= '9'; num++) {
-        if(isValidSudoku(board,i,j,num)) {
-            board[i][j] = num;
-            if(helper(board,i,j+1)) return 1;
-            board[i][j] = '.';
+class Solution {
+    bool isValidSudoku(vector<vector<char>>& board, int i, int j, char val) {
+        int row = i - i%3, column = j - j%3;
+        for(int x=0; x<9; x++) if(board[x][j] == val) return false;
+        for(int y=0; y<9; y++) if(board[i][y] == val) return false;
+        for(int x=0; x<3; x++) {
+            for(int y=0; y<3; y++) if(board[row+x][column+y] == val) return false;
         }
+        return true;
     }
-    return 0;
-}
-void solveSudoku(vector<vector<char> >& board) {
-    helper(board,0,0);
-}
-
-int main() {
-    vector<vector<char> > board(9,vector<char>(9));
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            cin>>board[i][j];
+    bool helper(vector<vector<char>>& board, int i, int j) {
+        if(i == 9) return 1;
+        if(j == 9) return helper(board,i+1,0);
+        if(board[i][j] != '.') return helper(board,i,j+1);
+        
+        for(char num = '1'; num <= '9'; num++) {
+            if(isValidSudoku(board,i,j,num)) {
+                board[i][j] = num;
+                if(helper(board,i,j+1)) return 1;
+                board[i][j] = '.';
+            }
         }
+        return 0;
     }
-    solveSudoku(board);
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            cout<<board[i][j]<<" ";
-        }
-        cout<<endl;
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board,0,0);
     }
-    return 0;
-}
+};
